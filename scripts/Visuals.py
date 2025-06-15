410fb25c4fb7f8f13aec001ec9a753df9af71759
nebiyuu <nebiyuessayas01@gmail.com>
2025-06-15 23:20:53 +0300

    renamed files for more discriptive names

--- scripts/Visuals.py
+++ scripts/Visuals.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def bars(series):
+def bars(series,title):
     plt.figure(figsize=(10, 6))
     sns.barplot(x=series.values, y=series.index, palette='viridis')
     
     plt.xlabel("Count")
     plt.ylabel("Bank")
-    plt.title("Bank Distribution", fontsize=14)
-    
+    plt.title(title, fontsize=14)
+
     for i, v in enumerate(series.values):
         plt.text(v + (v * 0.01), i, f"{v:,}", va='center')
     
